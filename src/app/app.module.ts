e29c7cecb92110f3c06ec1823a69d45a53b8bc4f
Rashid H Armand <rashidharmand@gmail.com>
2018-10-26 15:50:24 -0400

    restructured app to hide firebase data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from "@angular/fire/database";
-
 import { AppComponent } from './app.component';
 import { importType } from '@angular/compiler/src/output/output_ast';
 import { ChatComponent } from './chat/chat.component';
+import * as firebaseInfo from './private';
+
+let apiKey = firebaseInfo.apiKey;
+let authDomain = firebaseInfo.authDomain;
+let databaseURL = firebaseInfo.databaseURL;
+let projectId = firebaseInfo.projectId;
+let storageBucket = firebaseInfo.storageBucket;
+let messagingSenderId = firebaseInfo.messagingSenderId;
 
 export const environment = {
   production: false,
   firebase: {
-  // Initialize Firebase
-    apiKey: "AIzaSyCnIpzoTr0DV6uxqjvxFJh_YX9kRceAlQM",
-    authDomain: "testchat-d8acc.firebaseapp.com",
-    databaseURL: "https://testchat-d8acc.firebaseio.com",
-    projectId: "testchat-d8acc",
-    storageBucket: "testchat-d8acc.appspot.com",
-    messagingSenderId: "302466631910"
+    // Initialize Firebase
+    apiKey,
+    authDomain,
+    databaseURL,
+    projectId,
+    storageBucket,
+    messagingSenderId
   }
 };
 
